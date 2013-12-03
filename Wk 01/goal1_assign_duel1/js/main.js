f83e837722d086a45ae100631892e578208e7300
klarski <kristennl1@gmail.com>
2013-12-02 20:09:44 -0500

    start Duel-Part 1

---
+++ Wk 01/goal1_assign_duel1/js/main.js
@@ -0,0 +1,23 @@
+// Kristen Kozinski
+// 12/2/13
+// The Duel - Part 1
+
+
+var playerOneName = "Santa Claus";
+var playerTwoName = "Easter Bunny";
+
+var playerOneHealth = 100;
+var playerTwoHealth = 100;
+
+var playerOneDamage = 20;
+var playerTwoDamage = 20;
+
+var fight = function (){
+	for(var i = 0; i < 10, i++){
+
+	};
+};
+
+var winnerCheck = function(){
+
+};
